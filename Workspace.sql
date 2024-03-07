e85f594de4ab118964ba3f87014e3e8f43df6ab2
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-07 23:41:52 +0000

    Thu, Mar 7, 2024, 11:41 PM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -1,39 +1,67 @@
---Create a new Function
+-- Question 1 (5 marks)
+-- DECLARE
+--  lv_mytax_num NUMBER(3,4);
+-- BEGIN
+-- CASE 'ON'
+--  WHEN 'MN' THEN lv_mytax_num := .08;
+--  WHEN 'QB' THEN lv_mytax_num := .04;
+--  WHEN 'SK' THEN lv_mytax_num := .07;
+-- END CASE;
+-- DBMS_OUTPUT.PUT_LINE('tax rate = '||lv_mytax_num);
+-- END;
+-- a. Enter and run the block of code above and save a screen shot of the code and error in the
+-- output.
+-- b. Modify the code by adding an exception handle for the error message, with a custom
+-- message saying, “No tax found for this code block”. Save a screenshot of your modified
+-- code and output.
 
--- Start with function signature
+-- a. Enter and run the block of code above and save a screen shot of the code and error in the
+-- output.
+-- SQL> DECLARE
+--   2      lv_mytax_num NUMBER(3, 4);
+--   3  BEGIN
+--   4      CASE 'ON'
+--   5          WHEN 'MN' THEN
+--   6              lv_mytax_num := .08;
+--   7          WHEN 'QB' THEN
+--   8              lv_mytax_num := .04;
+--   9          WHEN 'SK' THEN
+--   10              lv_mytax_num := .07;
+--   11      END CASE;
+--   12
+--   13      DBMS_OUTPUT.PUT_LINE('tax rate = '
+--   14                           ||lv_mytax_num);
+--   15  END;
 
-CREATE FUNCTION FUNCTION1 (
-PARAM1 IN NUMBER) RETURN NUMBER IS
+-- DECLARE
+-- *
+-- ERROR at line 1:
+-- ORA-06592: CASE not found while executing CASE statement
+-- ORA-06512: at line 4
 
--- Declare constants and variables in this section.
--- Example: <Variable Identifier> <DATATYPE>
---          <Variable Identifier> CONSTANT <DATATYPE>
---          varEname  VARCHAR2(40);
---          varComm   REAL;
---          varSalary CONSTANT NUMBER:=1000;
---          comm_missing EXCEPTION;
-
--- Executable part starts here
+-- b. Modify the code by adding an exception handle for the error message, with a custom
+-- message saying, “No tax found for this code block”. Save a screenshot of your modified
+-- code and output.
+DECLARE
+    lv_mytax_num NUMBER(3, 4);
 BEGIN
+    CASE 'ON'
+        WHEN 'MN' THEN
+            lv_mytax_num := .08;
+        WHEN 'QB' THEN
+            lv_mytax_num := .04;
+        WHEN 'SK' THEN
+            lv_mytax_num := .07;
+ -- If the string is none of the above, raise an application error
+        ELSE
+            RAISE_APPLICATION_ERROR(-20001, 'No tax found for this code block');
+    END CASE;
 
-  -- Write PL/SQL and SQL statements to implement the processing logic
-  -- of subprogram. Example:
-  --     SELECT ENAME,
-  --            COMM
-  --     INTO   varEname,
-  --            varComm
-  --     FROM   EMP
-  --     WHERE  EMPNO = 7369;
-  --
-  --     IF varComm IS NULL THEN
-  --         RAISE comm_missing;
-  --     END IF;
-
-  RETURN PARAM1;
-
-  -- EXCEPTION -- exception-handling part starts here
-  -- WHEN comm_missing THEN
-  --   dbms_output.put_line('Commision is NULL');
-
-END FUNCTION1;
-/
+    DBMS_OUTPUT.PUT_LINE('tax rate = '
+                         ||lv_mytax_num);
+ -- If any error occurs, catch it and output the error message
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('An error occurred: '
+                             || SQLERRM);
+END;
